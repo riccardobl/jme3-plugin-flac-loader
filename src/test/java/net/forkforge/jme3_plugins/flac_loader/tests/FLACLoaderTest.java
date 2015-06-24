70e3d3dd28acf623be58ddb2055848b5e5b69488
Riccardo Balbo <riccardo@forkforge.net>
2015-06-24 14:54:14 +0200

    Updated pom

--- src/test/java/net/forkforge/jme3_plugins/flac_loader/tests/FLACLoaderTest.java
+++ src/test/java/net/forkforge/jme3_plugins/flac_loader/tests/FLACLoaderTest.java
@@ -24,6 +24,7 @@
  * 	OTHER DEALINGS IN THE SOFTWARE.
  * 	For more information, please refer to <http://unlicense.org>
  */
+
 package net.forkforge.jme3_plugins.flac_loader.tests;
 
 import net.forkforge.jme3_plugins.flac_loader.FLACLoader;
